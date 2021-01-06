0a142908d3feed37d9b0e80ede42c2d3ac6b60e4
AadeshShimpi <66712093+AadeshShimpi@users.noreply.github.com>
2021-01-06 17:11:57 +0530

    Demo Project

---
+++ AppDemo-1/src/main/java/net/javacode/AppDemo1Application.java
@@ -0,0 +1,13 @@
+package net.javacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDemo1Application.class, args);
+	}
+
+}
---
+++ AppDemo-1/src/test/java/net/javacode/AppDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javacode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
